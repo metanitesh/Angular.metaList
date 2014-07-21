9208eaab15ce6f6e5a41c42ecca52e15cc5d14ed
nitesh sharma <nitesh.grep@gmail.com>
2014-07-21 19:07:20 +0530

    adding app.js

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', []);
+
