8af37a329c34b0f8cea9f0cbd9e92c9d6b72ecf3
nitesh sharma <nitesh.grep@gmail.com>
2014-07-21 20:51:23 +0530

    updating app.js

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,5 @@
 
 
 // Declare app level module which depends on filters, and services
-'use strict';
-
-
-// Declare app level module which depends on filters, and services
 angular.module('myApp', []);
 
