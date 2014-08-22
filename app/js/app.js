d3b95c8584fdaa9e99b05d2b626bbc16f1c28dc1
nitesh sharma <nitesh.grep@gmail.com>
2014-08-22 12:31:33 +0530

    updating index.html and app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,42 +1,8 @@
 'use strict';
 
-var myApp = angular.module('myApp', []);
+var metaList = angular.module('metaList', []);
 
-myApp.provider('data', {
-	$get: function(){
-		return [{
-		title: "bucketList",
-		id: 1,
-		tasks: [{
-			title: "play soccer for world league",
-			id: 1,
-			done: false,
-			comments: ["fifa 2014 is about to start", "need to go buy a Brazil T-shirt"]
-		}, {
-			title: "start a school",
-			id: 2,
-			done: true,
-			comments: ["start with being a mentor"]
-		}]
-	}, {
-		title: "to-do",
-		id: 2,
-		tasks: [{
-			title: "create a todo App",
-			id: 1,
-			done: false,
-			comments: []
-		}, {
-			title: "watch GOT",
-			id: 2,
-			done: false,
-			comments: ["whitewalkers seems to be in no hurry"]
-		}]
-	}];
-	}
-})
-
-myApp.controller('MyCtrl1', ['$rootScope', '$scope', 'data', function($rootScope, $scope, data) {
+metaList.controller('MyCtrl1', ['$rootScope', '$scope', 'data', function($rootScope, $scope, data) {
 	$scope.items = data;
 
 	$scope.showTasks = function(item){
@@ -51,7 +17,8 @@ myApp.controller('MyCtrl1', ['$rootScope', '$scope', 'data', function($rootScope
 }])
 
 
-myApp.directive('ngEnter', function () {
+metaList.directive('ngEnter', function () {
+	
     return function (scope, element, attrs) {
     	console.log("in")
         element.bind("keydown keypress", function (event) {
