839716270ba08bc6a6a279dd597de5e1b30755ae
nitesh sharma <nitesh.grep@gmail.com>
2014-08-25 18:27:33 +0530

    adding show tasks method on listController

--- app/js/controllers/listController.js
+++ app/js/controllers/listController.js
@@ -1,13 +1,14 @@
-metaList.controller('listController', ['$rootScope', '$scope', 'data', function($rootScope, $scope, data) {
-	$scope.items = data;
+metaList.controller('listController', ['$rootScope', '$scope', 'data',
+	function($rootScope, $scope, data) {
+		$scope.items = data;
 
-	$scope.showTasks = function(item){
-		console.log("myc1", item)
-		// $rootScope.$broadcast('showTasks', item);;
-	}
 
-	$scope.doSomething = function(item){
-		window.alert("hello")	
-		// $scope.items.push({title: item});		
+		$scope.showTasks = function($index) {
+			$scope.selected = $index;
+		}
+
+		// $scope.addItem = function(item) {
+					
+		// }
 	}
-}])
+])
