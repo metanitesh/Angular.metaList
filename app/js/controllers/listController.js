54f78a4074298ee7093d9ab6d0ca29450e0f3b1f
nitesh sharma <nitesh.grep@gmail.com>
2014-08-25 18:46:22 +0530

    adding addItem method

--- app/js/controllers/listController.js
+++ app/js/controllers/listController.js
@@ -7,10 +7,20 @@ metaList.controller('listController', ['$rootScope', '$scope', 'data',
 			$scope.selected = $index;
 		}
 
-		$scope.deleteTask = function($index){
+		$scope.deleteTask = function($index) {
 			$scope.items.splice($index, 1);
 		}
-		
-		
+
+		$scope.addItem = function(item) {
+			
+			var newItem = {
+				title: $scope.newItemTitle,
+				id: Math.random(),
+				tasks: []
+			}
+
+			$scope.items.push(newItem);
+			$scope.newItemTitle = '';
+		}
 	}
 ])
