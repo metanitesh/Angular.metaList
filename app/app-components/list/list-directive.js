2f1df37cad8949a37f264e1087368b155f25f782
nitesh sharma <nitesh.grep@gmail.com>
2014-09-24 19:40:32 +0530

    updating list directive and template

--- app/app-components/list/list-directive.js
+++ app/app-components/list/list-directive.js
@@ -2,6 +2,7 @@ angular.module("metaList").directive("list", function(){
 
     return {
         restrict: "E",
+        replace: false,
         templateUrl: "app-components/list/list.html",
         controller: 'mlListCtrl'
     }    
