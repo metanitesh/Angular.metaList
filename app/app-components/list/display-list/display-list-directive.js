6db0e6669c8072dd200c8a8574867e2bc5f5d97c
nitesh sharma <nitesh.grep@gmail.com>
2014-09-24 19:40:54 +0530

    updating display-list directive and template

--- app/app-components/list/display-list/display-list-directive.js
+++ app/app-components/list/display-list/display-list-directive.js
@@ -1,9 +1,8 @@
-
 angular.module("metaList").directive("displayList", function(){
     
     return {
         restrict: "E",
-        templateUrl: "app-components/list/display-list/display-list.html"
+        templateUrl: "app-components/list/display-list/display-list.html",
     }    
 
 })
