74037a88da3739526039733b0b59e05a3eb8ec79
nitesh sharma <nitesh.grep@gmail.com>
2014-09-24 16:58:48 +0530

    adding underscore in karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
+      'app/bower_components/underscore/underscore.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
