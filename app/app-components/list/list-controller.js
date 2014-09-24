b9aac832962cc504ec81a535a53502c45b98f976
nitesh sharma <nitesh.grep@gmail.com>
2014-09-24 16:48:04 +0530

    adding removeList specs and implementation in mlListCtrl

--- app/app-components/list/list-controller.js
+++ app/app-components/list/list-controller.js
@@ -1,22 +1,77 @@
 metaList.controller('mlListCtrl', ['$scope', 'mlData', function($scope, mlData) {
 
-    $scope.lists = mlData.getLists()
+    $scope.lists = mlData.getLists();
 
-    $scope.selectList = function(list) {
 
-      $scope.selected = list.id;
+    $scope.selectList = function(id) {
+      $scope.activeListId = id;
     }
 
-    $scope.enableEditMode = function(list) {
-      $scope.enableEdit = list.id;
+    $scope.removeList = function(id){
+       mlData.removeListById(id);
     }
 
-    $scope.updateList = function(event, list) {
+    
+    // $scope.enableEditMode = function(list) {
+    //   $scope.enableEdit = list.id;
+    // }
 
-      if (event.which === 13) {
-        mlData.updateList(list);    
-      }
+    // $scope.updateList = function(list) {
+    //     console.log($scope.updatedValue);
+    //     mlData.updateList(list, $scope.updatedValue);
+    //     $scope.enableEdit = false;
 
-    }
-  }
-])
+      
+    // }
+
+    // $scope.stopPropagation = function(event){
+    //     event.stopPropagation();
+    // }
+
+    // $scope.removeList = function(id){
+    //    mlData.removeList(id); 
+    // }
+
+    // $scope.addList = function(){
+    //     // console.log($scope.newListTitle);
+    //     if($scope.newListTitle){
+    //         mlData.addList($scope.newListTitle); 
+    //         $scope.newListTitle = "";
+    //     }       
+        
+    // }
+  }])
+
+
+    // $scope.selectList = function(list) {
+    //   $scope.enableEdit = false;  
+    //   $scope.selected = list.id;
+    // }
+
+    // $scope.enableEditMode = function(list) {
+    //   $scope.enableEdit = list.id;
+    // }
+
+    // $scope.updateList = function(list) {
+    //     console.log($scope.updatedValue);
+    //     mlData.updateList(list, $scope.updatedValue);
+    //     $scope.enableEdit = false;
+
+      
+    // }
+
+    // $scope.stopPropagation = function(event){
+    //     event.stopPropagation();
+    // }
+
+    // $scope.removeList = function(index){
+    //    mlData.removeList(index); 
+    // }
+
+    // $scope.addList = function(){
+    //     if($scope.newListTitle){
+    //         mlData.addList($scope.newListTitle); 
+    //         $scope.newListTitle = "";
+    //     }       
+        
+    // }
--- test/unit/list/listcontrollerspec.js
+++ test/unit/list/listcontrollerspec.js
@@ -3,7 +3,7 @@ describe("list controller", function() {
   var scope;
   var controller;
 
-  var mlDataMock = [{
+  var ListDbMock = [{
     title: "books",
     id: 1
     }, {
@@ -16,14 +16,15 @@ describe("list controller", function() {
 
   var mlDataServiceMock = {
     getLists: function() {
-      return mlDataMock
+      return ListDbMock
     },
-    updateList: function() {
-      return true;
+    removeListById: function() {
+      return;
     }
   }
 
   beforeEach(module("metaList"));
+
   beforeEach(function() {
     inject(function($rootScope, $controller) {
 
@@ -33,56 +34,67 @@ describe("list controller", function() {
     })
   });
 
-  beforeEach(function(){
+  beforeEach(function() {
     controller("mlListCtrl", {
       $scope: scope,
       mlData: mlDataServiceMock
-    }) 
+    })
   })
 
   it(":lists, should setup lists in scope", function() {
 
-    
+
     /*then*/
-    expect(scope.lists).toEqual(mlDataMock);
+    expect(scope.lists).toEqual(ListDbMock);
 
   });
 
   it(":selectList, should apply active class to selected list item", function() {
 
-    
-   
+
     /*when*/
-    scope.selectList(mlDataMock[0]);
+    scope.selectList(1);
+
     /*then*/
-    expect(scope.selected).toEqual(1);
+    expect(scope.activeListId).toEqual(1);
 
   })
 
-  it("enableEditMode:, should enable edit mode for selected list item", function(){
+  it(":removeList, should removeList from listDb", function() {
 
-    
+    /*setup*/
+    spyOn(mlDataServiceMock, "removeListById")
+
     /*when*/
-    scope.enableEditMode(mlDataMock[0]);
+    scope.removeList(1);
+
     /*then*/
-    expect(scope.enableEdit).toEqual(1);
-    
+    expect(mlDataServiceMock.removeListById).toHaveBeenCalledWith(1);
   })
+  // it("enableEditMode:, should enable edit mode for selected list item", function(){
 
-  it("updateList:, should call mlDataService update method with selected list item", function(){
 
-     /*setup*/
-     var event =  {
-      which: 13
-     }
+  //   /*when*/
+  //   scope.enableEditMode(mlDataMock[0]);
+  //   /*then*/
+  //   expect(scope.enableEdit).toEqual(1);
 
-     spyOn(mlDataServiceMock, 'updateList');
-    
-     /*when*/
-    scope.updateList(event, mlDataMock[0]);
+  // })
 
-    expect(mlDataServiceMock.updateList).toHaveBeenCalledWith(mlDataMock[0]);
-    
-  })
+  // it("updateList:, should call mlDataService update method with selected list item", function(){
+
+  //    /*setup*/
+  //    var event =  {
+  //     which: 13
+  //    }
+
+  //    spyOn(mlDataServiceMock, 'updateList');
+
+  //    /*when*/
+  //   scope.updateList(event, mlDataMock[0]);
+
+  //   expect(mlDataServiceMock.updateList).toHaveBeenCalledWith(mlDataMock[0]);
+
+  // })
 
 });
