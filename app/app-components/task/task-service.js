22bbfd3b1b805f1beff0d4c303d92166237ca9de
nitesh sharma <nitesh.grep@gmail.com>
2014-09-25 18:05:38 +0530

    adding task service spec

---
+++ app/app-components/task/task-service.js
@@ -0,0 +1,38 @@
+angular.module("metaList").factory('mlTaskService', ["mlData", "mlIdGenerator", function(mlData, mlIdGenerator) {
+
+    "use strict";
+
+    var getTaskFor = function(listId) {
+      return mlData.getListById(listId).tasks;
+    };
+
+    var addTaskTo = function(listId, config) {
+
+      if (!config.title) {
+        throw "to create a new task title is required";
+      }
+
+      var newTask = {
+        id: mlIdGenerator.getId(),
+        title: config.title
+      };
+
+      var tasks = getTaskFor(listId);
+      tasks.push(newTask);
+    };
+
+    var removeTaskFrom = function(listId, taskId) {
+      var tasks = getTaskFor(listId);
+      for (var i = tasks.length - 1; i >= 0; i--) {
+        if (tasks[i]["id"] === taskId)
+          tasks.splice(i, 1);
+      }
+    }
+
+    return {
+      getTasksFor: getTaskFor,
+      addTaskTo: addTaskTo,
+      removeTaskFrom: removeTaskFrom
+    };
+
+}]);
