853737aeb0b74559b845a4b4945fb53a6cb53e4b
nitesh sharma <nitesh.grep@gmail.com>
2014-09-26 12:17:59 +0530

    adding display task html and directive

---
+++ app/app-components/task/display-task/display-task-directive.js
@@ -0,0 +1,8 @@
+angular.module("metaList").directive("displayTask", function(){
+    
+    return {
+        restrict: "E",
+        templateUrl: "app-components/task/display-task/display-task.html",
+    }    
+
+})
