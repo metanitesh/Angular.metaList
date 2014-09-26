975a80317fe1e8e1cddfd878aade75a9477e8b0f
nitesh sharma <nitesh.grep@gmail.com>
2014-09-26 19:41:43 +0530

    setting up karma to pass travis

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function(config) {
   config.set({
-    basePath: '',
+    basePath: '../',
 
     frameworks: ['jasmine'],
 
@@ -28,7 +28,7 @@ module.exports = function(config) {
 
     },
 
-
+    singleRun: true,
 
     browsers: ['PhantomJS'],
 
