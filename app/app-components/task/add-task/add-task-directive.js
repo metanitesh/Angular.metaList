4c7eb619af343ac9b40fb5294a06ce57c4217e61
nitesh sharma <nitesh.grep@gmail.com>
2014-09-26 12:18:14 +0530

    adding add task directive and html

---
+++ app/app-components/task/add-task/add-task-directive.js
@@ -0,0 +1,8 @@
+angular.module("metaList").directive("addTask", function(){
+    
+    return {
+        restrict: "E",
+        templateUrl: "app-components/task/add-task/add-task.html",
+    }    
+
+})
