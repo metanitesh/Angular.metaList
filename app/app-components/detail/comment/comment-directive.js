e12579e1f27dd4b6a5cbcaebb2b4b1be06d0cfe6
nitesh sharma <nitesh.grep@gmail.com>
2014-09-27 11:56:04 +0530

    adding comment directive

---
+++ app/app-components/detail/comment/comment-directive.js
@@ -0,0 +1,10 @@
+angular.module("metaList").directive("comment", function(){
+
+    return {
+        restrict: "E",
+        replace: false,
+        templateUrl: "app-components/detail/comment/comment.html",
+        
+    }    
+
+})
