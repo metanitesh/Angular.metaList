1c686bc393a322b8f5bfa35c2bc3de4ac20b10c9
nitesh sharma <nitesh.grep@gmail.com>
2014-09-27 11:56:47 +0530

    adding note directive

---
+++ app/app-components/detail/note/note-directive.js
@@ -0,0 +1,10 @@
+angular.module("metaList").directive("note", function(){
+
+    return {
+        restrict: "E",
+        replace: false,
+        templateUrl: "app-components/detail/note/note.html",
+        
+    }    
+
+})
