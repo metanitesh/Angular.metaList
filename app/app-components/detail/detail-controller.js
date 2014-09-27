dcba71dabd373b0cad1ff1552819d9f5d087de9a
nitesh sharma <nitesh.grep@gmail.com>
2014-09-27 17:01:12 +0530

    updating detail-controller

--- app/app-components/detail/detail-controller.js
+++ app/app-components/detail/detail-controller.js
@@ -3,11 +3,18 @@ metaList.controller('mlDetailCtrl', ['$scope', 'mlRouteParam', 'mlTaskService', 
     $scope.$watch(function() {
       return mlRouteParam.getParam().taskId;
     }, function() {
-        
+        $scope.listId =  mlRouteParam.getParam().listId;
+        $scope.taskId =  mlRouteParam.getParam().taskId;
         $scope.task = mlTaskService.getTaskById(mlRouteParam.getParam().listId, mlRouteParam.getParam().taskId);
-        
+        $scope.contentText=  $scope.task.content
     })
 
-    
+     $scope.addComment = function(newComment){
+        mlTaskService.addComment($scope.listId, $scope.taskId, newComment);
+        $scope.newComment = "";
+    }    
 
+    $scope.updateContent = function(newConent){
+        mlTaskService.updateContent($scope.listId, $scope.taskId, newConent);
+    }
 }])
