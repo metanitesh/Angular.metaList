39dbe34ef7ccd7fc15c56a1d8beb64791879cef9
nitesh sharma <nitesh.grep@gmail.com>
2014-09-29 17:31:30 +0530

    adding ml-app-storage and ml-app-storage spec

---
+++ test/unit/storage/ml-app-storage-spec.js
@@ -0,0 +1,55 @@
+describe('data service', function() {
+
+  
+  beforeEach(module('metaList'));
+
+  beforeEach(function(){
+    localStorage.setItem("testDb", JSON.stringify({list: "movie"}))
+  })
+
+  beforeEach(module(function($provide) {
+    $provide.value("mlBootstrapData", {list: "todo"})
+  }));
+
+  it('is sane', inject(function(mlStorage) {
+
+    /*then*/
+    expect(mlStorage).toBeDefined();
+  }));
+
+
+  it("init, should intialize db and set bootstrapData", inject(function(mlStorage) {
+    
+    /*setup*/  
+    mlStorage.setDbName("newTestDb");
+     
+    /*when*/  
+    mlStorage.init();
+    data = mlStorage.getData();
+
+    /*then*/
+    expect(data).toEqual({list: "todo"})
+    
+    /*after*/
+    localStorage.removeItem("newTestDb")
+    localStorage.removeItem("testDb");
+  }));
+
+
+  it("init, should use if db is available in localstorage", inject(function(mlStorage) {
+    
+    /*setup*/  
+    mlStorage.setDbName("testDb");
+    mlStorage.init();
+
+    /*when*/  
+    data = mlStorage.getData();
+
+    /*then*/  
+    expect(data).toEqual({list: "movie"})
+
+    /*after*/
+    localStorage.removeItem("testDb");
+  }));
+
+});
