7be92065a20fd7be342960ca8dfc1a776954cb7b
nitesh sharma <nitesh.grep@gmail.com>
2014-10-01 00:45:36 +0530

    updating taskController spec

--- test/unit/task/task-controller-spec.js
+++ test/unit/task/task-controller-spec.js
@@ -3,35 +3,6 @@ describe("task controller", function() {
   var scope;
   var controller;
 
-
-  var listMock = {
-    title: "todo",
-    id: 1,
-    tasks: [
-      {
-        "id": "6FB00942-3020-4DF7-8080-5881B4B17442",
-        "title": "clean house",
-        "comments": ["after lunch", "maybe tomorrow", "doesn't look that bad"],
-        "content": "it's been ages man !",
-        "done": true
-      },
-      {
-        "id": "508EE1CB-1544-429F-89FE-3739154448E2",
-        "title": "get a haircut",
-        "comments": ["will do this weekend"],
-        "content": "this could be a good reason to become a monk",
-        "done": true
-      },
-      {
-        "id": "F26660FB-C38C-4884-915E-B6F661332292",
-        "title": "buy grocery",
-        "comments": [],
-        "content": "1. eggs\n2. apples\n3. bread \n4. wine",
-        "done": false
-    }]
-  }
-
-
   var mlTaskServiceMock = {
 
     removeTask: function(taskId) {
