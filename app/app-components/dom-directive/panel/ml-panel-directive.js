e1720b2e3a8db46131e5eedce69a7115fb255be9
nitesh sharma <nitesh.grep@gmail.com>
2014-10-01 15:00:20 +0530

    adding panel directive

---
+++ app/app-components/dom-directive/panel/ml-panel-directive.js
@@ -0,0 +1,10 @@
+"use strict";
+
+angular.module("metaList").directive("panel", function() {
+
+	return {
+		restrict: "E",
+		templateUrl: "app-components/dom-directive/panel/ml-panel.html",
+	};
+
+});
