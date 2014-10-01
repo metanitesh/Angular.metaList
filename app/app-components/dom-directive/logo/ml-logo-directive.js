5ae86c2ea7d75f09cf9fa7a5605e67c9c41a4ce2
nitesh sharma <nitesh.grep@gmail.com>
2014-10-01 15:00:37 +0530

    adding logo directive and updating index

---
+++ app/app-components/dom-directive/logo/ml-logo-directive.js
@@ -0,0 +1,10 @@
+"use strict";
+
+angular.module("metaList").directive("logo", function() {
+
+	return {
+		restrict: "E",
+		templateUrl: "app-components/dom-directive/logo/ml-logo.html",
+	};
+
+});
